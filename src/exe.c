090fc1b7fff9d39bdb6c1c528e7d1590255d9215
Gustavo Campos <148006984+GustavoH-C@users.noreply.github.com>
2024-11-18 15:05:43 -0300

    Update exe.c

--- src/exe.c
+++ src/exe.c
@@ -1,37 +1,38 @@
-
 #include <stdio.h>
 #include <omp.h>
-#include "../include/trab.h"
+#include "trab.h"
 
-void competicao(int conta, int vencedor, int tamanho){
-int num_threads = 4;  // Podemos alterar para mais ou menos threads
+int contador = 0;
+int vencedor = -1;
+
+void competicao(int num_threads, int max)
+{
+
     omp_set_num_threads(num_threads);
 
-
-    // Região paralela
-    #pragma omp parallel
+#pragma omp parallel
     {
-        while (1) {
-            #pragma omp critical
+        while (1)
+        {
+#pragma omp critical
             {
-                if (conta < tamanho) {
-                    conta++;
+                if (contador < max)
+                {
+                    contador++;
 
-                    printf("Thread %d contou até %d\n", omp_get_thread_num(), conta);
+                    printf("Thread %d contou até %d\n", omp_get_thread_num(), contador);
 
-                    if (conta == tamanho && vencedor == -1) {
+                    if (contador == max && vencedor == -1)
+                    {
                         vencedor = omp_get_thread_num();
                     }
                 }
             }
 
-            if (conta >= tamanho) {
+            if (contador >= max)
+            {
                 break;
             }
         }
     }
-
-
-    printf("A thread vencedora foi a número %d.\n", vencedor);
-
 }
